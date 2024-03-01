e11b0ce7b2c144cfe34711765bbab5dda28640f8
Umesh Kumaar <umeshkumaar015@gmail.com>
2024-03-01 14:55:30 +0530

    Create test_code.c

---
+++ test_code.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct complex
+{
+    int real;
+    int imag;
+};
+
+void func1()
+{
+    int number = 10;
+    float cgpa = 8.5;
+    struct complex obj;
+}
+
+void func2(int a,int b)
+{
+    int value = a+b;
+    int arr[10] = {3,4,5,6};
+    double height;
+}
+
+int main()
+{
+    int meo = 10;
+    func1();
+    func2(5, 10);
+    return 0;
+}
